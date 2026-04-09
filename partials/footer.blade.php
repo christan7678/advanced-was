c3a362333c0bfac1f08c8a874294cbe5e2095b99
jingyin044-cloud <jingyin044@gmail.com>
2026-04-09 09:25:40 +0800

    Update footer.blade.php

--- partials/footer.blade.php
+++ partials/footer.blade.php
@@ -26,7 +26,7 @@
 
             {{-- Bookings column --}}
             <div class="footer-col">
-                <h4>Bookings</h4>
+                <h4>Qucik Link</h4>
                 <a href="{{ route('events.index') }}">
                     <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5"><rect x="3" y="4" width="18" height="18" rx="2"/><path d="M16 2v4M8 2v4M3 10h18"/></svg>
                     Browse events
